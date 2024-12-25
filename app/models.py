de68103340dda4e0cf67ecb1f86b8ebf26c5a5a6
kataNjk <katanosakatest@gmail.com>
2024-12-25 11:02:10 +0900

    モデルに値についてのコメントを追加

--- app/models.py
+++ app/models.py
@@ -10,11 +10,14 @@ class ContactEmail(db.Model):  # type: ignore
     __tablename__: str = "contact_emails"
 
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 問い合わせ種別
+    # （1: 採用情報について, 2: 取り扱い製品について, 3: 事業内容について, 4: プライバシーポリシーについて, 5: その他）
     contact_type: int = db.Column(db.Integer, nullable=False)
     content: str = db.Column(db.Text, nullable=False)
     name: str = db.Column(db.String(200), nullable=False)
     kana: str = db.Column(db.String(200), nullable=False)
     email: str = db.Column(db.String(200), nullable=False)
+    # 性別（1: 男性, 2: 女性）
     gender: int = db.Column(db.Integer, nullable=False)
     ip: str = db.Column(db.String(200), nullable=False)
     received_at: datetime = db.Column(db.DateTime, nullable=False, index=True, default=Config.now_jst)
